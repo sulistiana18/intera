ba2897245cd2c56bd7411f56d16c4c87445fd232
sulistiana18 <suliez.tiana16@gmail.com>
2026-05-23 13:18:15 +0700

    2nd build

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,18 @@
 import type { NextConfig } from "next";
 
-const basePath = process.env.NODE_ENV === "production" ? "/intera" : "";
+const basePath = "/intera";
 
 const nextConfig: NextConfig = {
   output: "export",
+
   basePath,
   assetPrefix: basePath,
+
   images: {
     unoptimized: true,
   },
+
   trailingSlash: true,
-  env: {
-    NEXT_PUBLIC_BASE_PATH: basePath,
-  },
 };
 
 export default nextConfig;
