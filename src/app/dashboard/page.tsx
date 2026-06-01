fe5af391995896f2737b6098ceb1edeb16e4e056
sulistiana18 <suliez.tiana16@gmail.com>
2026-06-01 12:50:58 +0700

    test vercel

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,4 +2,11 @@ import Dashboard from "@/components/Dashboard";
 
 export default function DashboardPage() {
   return <Dashboard />;
+}// src/app/dashboard/page.tsx
+import Dashboard from "@/components/Dashboard";
+
+export const dynamic = "force-dynamic";
+
+export default function DashboardPage() {
+  return <Dashboard />;
 }
