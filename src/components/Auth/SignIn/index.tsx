2128c86fceb592ec397b486d2220ea7b5c41f6a2
sulistiana18 <suliez.tiana16@gmail.com>
2026-06-01 17:12:35 +0700

    edit redirect

--- src/components/Auth/SignIn/index.tsx
+++ src/components/Auth/SignIn/index.tsx
@@ -39,7 +39,7 @@ const Signin = ({ signInOpen }: Props) => {
     const { error } = await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
-        redirectTo: `${window.location.origin}/dashboard`,
+        redirectTo: `${window.location.origin}`,
       },
     });
 
